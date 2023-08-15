d5b66c09875d49a1cefd870bf36330a925e8c194
Wishvanath Adhikari <buddhiwishvanath123@gmail.com>
2023-08-15 23:01:58 +0530

    Initial commit

---
+++ SmartBankingCliApp.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class SmartBankingCliApp{
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+}
